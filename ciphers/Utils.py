17b12ea1ddb9ee7783179b422c5ea696a22d6c74
klettgau <klettgaudeux@gmail.com>
2020-05-31 13:49:18 -0400

    Added function to convert char to int representation

--- ciphers/Utils.py
+++ ciphers/Utils.py
@@ -1,6 +1,12 @@
 # this will be for commonly used functions.
-def char_to_int(msg: str) -> list:
+def char_to_int_li(msg: str) -> list:
     return [ord(s)-65 for s in msg]
+
+def char_to_int(msg:str)->int:
+    return ord(msg.upper())-65
+
+def list_to_str(msg):
+    return ''.join(msg)
 
 
 def int_to_char(msg: list) -> str:
