91491c243d97e401bc2dcceb7f4ecaca9e691025
klettgau <klettgaudeux@gmail.com>
2020-05-31 13:50:13 -0400

    Modifed test_rule1 and double detection

--- tests/test_playfair.py
+++ tests/test_playfair.py
@@ -5,40 +5,29 @@ import ciphers.Utils as util
 
 class PlayFairTest(unittest.TestCase):
 
-    def test_pad_message(self):
-        first_msg = util.char_to_int("")
-        second_msg = util.char_to_int("spaceforce")
-        third_msg = util.char_to_int("one")
-        fourth_msg = util.char_to_int("I")
-        first_mod = pf.pad_message(first_msg)
-        self.assertEqual(len(first_mod), 2)
-        second_mod = pf.pad_message(second_msg)
-        self.assertEqual(len(second_mod), 10)
-        third_mod = pf.pad_message(third_msg)
-        self.assertEqual(len(third_mod), 4)
-        four_mod = pf.pad_message(fourth_msg)
-        self.assertEqual(len(four_mod), 2)
-
     def test_double_dection(self):
         base_msg="MOSESSUPPOSES"
-        msg=pf.pad_message(util.char_to_int(base_msg))
-        updated_msg=pf.detect_double(msg)
+        updated_msg=pf.detect_double(util.char_to_int(base_msg))
         print(updated_msg)
-        print(len(updated_msg))
-        self.assertEqual(len(updated_msg),8)
-        
+        self.assertEqual(len(updated_msg),7)
+
     # def test_which_rule(self):
     #
     def test_rule1(self):
         # Only called when input has same letter in a row
         # Assumes empty strings etc are handled upstream
-        first_msg = util.char_to_int("x")
+        golden_a=(23,23)
+        golden_b=(0,23)
+        first_msg = util.char_to_int("X")
         second_msg = util.char_to_int("A")
 
         first_mod = pf.rule_one(first_msg)
         second_mod = pf.rule_one(second_msg)
         self.assertEqual(len(first_mod), 2)
+        self.assertEqual(golden_a,first_mod)
+
         self.assertEqual(len(second_mod), 2)
+        self.assertEqual(golden_b,second_mod)
 
     # def test_rule2(self):
     #
